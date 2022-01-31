898ff9446d76180fab4274ef56f55056786db24e
Scorpion197 <doudou.gaw@gmail.com>
2022-01-31 22:04:18 +0100

    added bookstore challenge

---
+++ alphaCTF/pwn/bookstore/chall.c
@@ -0,0 +1,163 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 512 
+#define MAX_ENTRIES 10
+
+void disable_buffering();
+void add_book();
+void remove_book();
+void edit_book(); 
+void menu(); 
+int get_option();
+
+//BSS variables 
+char *entries[MAX_ENTRIES];
+int created_entries = 0;
+
+int main(int argc, char *argv[]) {
+
+    disable_buffering();
+
+    printf("Here is your leak: %p\n", printf);
+
+
+    int option = 0; 
+
+    while (1) {
+
+        menu();
+        option = get_option();
+
+        switch(option) {
+
+            case 1:
+                add_book();
+                break; 
+
+            case 2:
+                edit_book(); 
+                break; 
+
+            case 3:
+                remove_book();
+                break; 
+
+            case 4:
+                exit(0);
+                break; 
+
+            default:
+
+                printf("Invalid option");
+                break; 
+        }
+
+
+    }
+
+    return 0;
+}
+
+void disable_buffering() {
+
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
+}
+void menu() {
+
+    puts("1- Add Book");
+    puts("2- Edit Book");
+    puts("3- Remove book");
+    puts("4- Exit");
+
+}
+
+int get_option() {
+
+    
+    int option; 
+    int c = 0; 
+
+    printf("Enter an option: ");
+
+    scanf("%d", &option);
+
+    // get rid of new line
+    c = getchar();
+    return option;
+
+}
+
+void add_book() {
+
+    fflush(stdin);
+
+    int c = 0;
+    char size_buffer[7]; 
+    int size = 0; 
+    char *ptr = NULL; 
+    int input_length = 0;
+
+    // MAX_ENTRIES - 1 just to avoid a BSS overflow
+    if (created_entries > MAX_ENTRIES - 1) {
+
+        printf("Maximum books reached!\n");
+        return;
+    }
+
+    
+    printf("Size: ");
+    fgets(size_buffer, 5, stdin);
+    
+    size = atoi(size_buffer);
+
+    if (size <= 0 || size > MAX_SIZE) {
+
+        printf("Invalid size\n");
+        return;
+    }
+
+    ptr = malloc(sizeof(char) * size);
+
+    printf("Content: ");
+
+    input_length = read(0, ptr, size);
+
+    // overflow of two bytes in the 'next size' field
+    ptr[input_length+1] = '\0';
+
+    entries[created_entries] = ptr;
+
+    created_entries++;
+
+    printf("Book added\n");
+
+}
+
+void edit_book() {
+
+    printf("Edit book\n\n");
+}
+
+void remove_book() {
+
+    int index = 0; 
+
+    printf("Book index: "); 
+    scanf("%d", &index);
+
+    if (index < 0 || index > MAX_ENTRIES ) {
+
+        printf("Invalid index\n");
+        return; 
+    }
+
+    free(entries[index]);
+    memset(entries[index], 0 ,8);
+
+    printf("Book removed!\n");
+}
