af9688d5007b03c34addc5a95050649dd715c06c
Gaouaoui Kamel <doudou.gaw@gmail.com>
2022-02-01 20:13:21 +0100

    Update chall.c

--- alphaCTF/pwn/messy_malloc/chall.c
+++ alphaCTF/pwn/messy_malloc/chall.c
@@ -2,38 +2,43 @@
 #include <stdio.h>
 #include <string.h>
 
-void disable_buffering();
+#define MAX_LENGTH 2048
 
 int main(int argc, char *argv[]) {
 
-    disable_buffering();
+    setbuf(stdout, NULL);
 
-    size_t size; 
-    void *pointer;
+    size_t size = 0;
+    int *ptr = NULL;
+    char buffer[MAX_LENGTH + 3];
+
+    unsigned int content = 0;
+    char line;
+
 
     printf("Welcome to messy malloc \n");
-     
+    printf("Give us a welcoming message: "); 
+
+    fgets(buffer, MAX_LENGTH, stdin);
+    printf("Thank you let's start now!\n");
+
     printf("How much: "); 
     scanf("%ld", &size);
 
-    pointer = malloc(size);
-    printf("Allocated memory at: %p\n", pointer);
+    ptr = malloc(size);
+    printf("Allocated memory at: %p\n", ptr);
 
     printf("Where ?: ");
     scanf("%ld", &size);
 
     printf("Content: ");
-    scanf("%zu",pointer + (size *8));
+    
+    scanf("%zu", (ptr + (size * 8)));
 
-    printf("See ya ^^ \n");
+    printf("%s", buffer);
+
 
     return 0;
 
 }
 
-void disable_buffering() {
-
-    setbuf(stdin, NULL);
-    setbuf(stdout, NULL);
-    setbuf(stderr, NULL);
-}
